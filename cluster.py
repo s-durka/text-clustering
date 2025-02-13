a299f52ccbbc898a974e8511d557db1d5f70e79d
Stanisław Durka <stas.durka@gmail.com>
2025-02-13 17:22:54 +0100

    refactor: split code into shorter functions

--- cluster.py
+++ cluster.py
@@ -32,48 +32,59 @@ def normalize_images(images: np.ndarray, eps=10, resize_to=60):
     WHITE = 255
     normalized_images = []
 
-    for i in range(len(images)):
-        img = images[i]
-
-        n_rows = img.shape[0]
-        n_cols = img.shape[1]
-    
-        # columns left to right:
-        for col in range (n_cols):
-            if WHITE - np.min(img[:,col]) < eps:
-                img = np.delete(img, col, axis=1)
-            else:
-                break
-
-        # columns right to left:
-        for col in range (n_cols - 1, -1, -1):
-            if WHITE - np.min(img[:,col]) < eps:      # if entire column is white (- epsilon),
-                img = np.delete(img, col, axis=1)     # then remove it
-            else:
-                break
-
-        # rows top to bottom
-        for row in range(n_rows):
-            if WHITE - np.min(img[row]) < eps:
-                img = np.delete(img, row, axis=0)
-            else:
-                break
-            
-        # rows bottom to top
-        for row in range(n_rows -1, -1, -1):
-            if WHITE - np.min(img[row]) < eps:
-                img = np.delete(img, row, axis=0)
-            else:
-                break
-
-        pillow_img = Image.fromarray(img, 'L')
-        resized_pillow_img = pillow_img.resize((resize_to, resize_to), Image.LANCZOS)
-        resized_img = np.asarray(resized_pillow_img)
-        flat_img = resized_img.flatten()
-        flat_img = np.concatenate((flat_img, [img.shape[0], img.shape[1], img.shape[0]/img.shape[1]]))
+    for img in images:
+        img = crop_image(img, eps, WHITE)
+        img = resize_image(img, resize_to)
+        flat_img = flatten_image(img)
         normalized_images.append(flat_img)
 
     return StandardScaler().fit_transform(np.array(normalized_images))
+
+
+def crop_image(img, eps, WHITE):
+    n_rows, n_cols = img.shape
+
+    # Crop columns left to right
+    for col in range(n_cols):
+        if WHITE - np.min(img[:, col]) < eps:
+            img = np.delete(img, col, axis=1)
+        else:
+            break
+
+    # Crop columns right to left
+    for col in range(n_cols - 1, -1, -1):
+        if WHITE - np.min(img[:, col]) < eps:
+            img = np.delete(img, col, axis=1)
+        else:
+            break
+
+    # Crop rows top to bottom
+    for row in range(n_rows):
+        if WHITE - np.min(img[row]) < eps:
+            img = np.delete(img, row, axis=0)
+        else:
+            break
+
+    # Crop rows bottom to top
+    for row in range(n_rows - 1, -1, -1):
+        if WHITE - np.min(img[row]) < eps:
+            img = np.delete(img, row, axis=0)
+        else:
+            break
+
+    return img
+
+
+def resize_image(img, resize_to):
+    pillow_img = Image.fromarray(img, 'L')
+    resized_pillow_img = pillow_img.resize((resize_to, resize_to), Image.LANCZOS)
+    return np.asarray(resized_pillow_img)
+
+
+def flatten_image(img):
+    flat_img = img.flatten()
+    flat_img = np.concatenate((flat_img, [img.shape[0], img.shape[1], img.shape[0] / img.shape[1]]))
+    return flat_img
 
 
 def create_output_list(labels, paths):
@@ -113,22 +124,7 @@ def create_output_files(lists):
         out.write("</html>\n")
 
 
-if __name__ == '__main__':
-    MAX_CLUSTERS = 100
-    paths = get_paths(sys.argv[1])
-    print(len(paths))
-    images = get_images(paths)
-
-
-    X = normalize_images(images) # flattened vectors with pixel values
-
-    pca = PCA(n_components=0.98)
-    X = pca.fit_transform(X)
-    new_n = np.cumsum(len(pca.explained_variance_ratio_))
-    print("new number of components:", new_n)
-
-    # find the best number of clusters for the K-Means algorithm
-    sse = []
+def find_best_k(X, MAX_CLUSTERS):
     list_k = list(range(2, MAX_CLUSTERS, 20))
     best_sscore = 0
     second_best_ss = 0
@@ -145,16 +141,15 @@ if __name__ == '__main__':
             best_k = k
         elif ss >= second_best_ss:
             second_best_ss = ss
-            second_best_k = k       
+            second_best_k = k
 
     print("best K: ", best_k)
     print("second best K: ", second_best_k)
 
-    # narrow down the search for the optimal number of clusters
     fromk = min(second_best_k, best_k)
-    fromk = max(2, fromk -10)
+    fromk = max(2, fromk - 10)
     tok = max(second_best_k, best_k)
-    tok = min(MAX_CLUSTERS -1, tok+10)
+    tok = min(MAX_CLUSTERS - 1, tok + 10)
     list_k2 = list(range(fromk, tok))
     for k in list_k2:
         km = KMeans(n_clusters=k, n_init='auto')
@@ -164,6 +159,23 @@ if __name__ == '__main__':
             best_sscore = ss
             best_k = k
 
+    return best_k
+
+
+def main(dataset_path):
+    MAX_CLUSTERS = 100
+    paths = get_paths(dataset_path)
+    print(len(paths))
+    images = get_images(paths)
+
+    X = normalize_images(images)  # flattened vectors with pixel values
+
+    pca = PCA(n_components=0.98)
+    X = pca.fit_transform(X)
+    new_n = np.cumsum(len(pca.explained_variance_ratio_))
+    print("new number of components:", new_n)
+
+    best_k = find_best_k(X, MAX_CLUSTERS)
     print("best number of clusters for the K-Means algorithm:", best_k)
 
     km = KMeans(n_clusters=best_k, n_init='auto')
@@ -172,3 +184,7 @@ if __name__ == '__main__':
     lists = create_output_list(labels, paths)
     create_output_files(lists)
     print(len(lists))
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])
